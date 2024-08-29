b89773676d05a1f9a2d2089ac0faf7cde92c8e38
Gustavo M <gutamvol262@gmail.com>
2024-08-28 19:10:41 -0600

    initial

---
+++ src/main/java/gfc/gesationdeinvetario/GesationDeInvetarioApplication.java
@@ -0,0 +1,13 @@
+package gfc.gesationdeinvetario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesationDeInvetarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesationDeInvetarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gfc/gesationdeinvetario/GesationDeInvetarioApplicationTests.java
@@ -0,0 +1,13 @@
+package gfc.gesationdeinvetario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesationDeInvetarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
