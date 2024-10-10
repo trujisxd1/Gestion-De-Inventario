f213e4bb6c553b5ce56f27cddf0019d7fc33ef6b
Gustavo M <gutamvol262@gmail.com>
2024-10-09 19:04:51 -0600

    pagina donde se muestra el detalle de el proveedor

---
+++ src/app/proveedor/pages/proveedor-pages/proveedor-pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedor-pages',
+  templateUrl: './proveedor-pages.component.html',
+  styleUrl: './proveedor-pages.component.css'
+})
+export class ProveedorPagesComponent {
+
+}
