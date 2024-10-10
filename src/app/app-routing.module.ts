2e4559255b26d36136b1f33d2b781773deac13b6
Gustavo M <gutamvol262@gmail.com>
2024-10-09 18:41:55 -0600

    haciendo carga peresosa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'categorias',
+    loadChildren:()=>import('./categoria/categoria.module').then(m=>m.CategoriaModule)
+  },
+  {
+    path:'productos',
+    loadChildren:()=>import('./productos/productos.module').then(m=>m.ProductosModule)
+  },
+  {
+    path:'proveedor',
+    loadChildren:()=>import('./proveedor/proveedor.module').then(m=>m.ProveedorModule)
+  },
+  {
+    path:'transaccion',
+    loadChildren:()=>import('./transaccion/transaccion.module').then(m=>m.TransaccionModule)
+  },
+  {
+    path:'',
+    redirectTo:'productos',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
