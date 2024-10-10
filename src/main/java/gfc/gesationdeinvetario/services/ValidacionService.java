2b309e2792052ef17ac8b3cdcdff58775d66ebd9
Gustavo M <gutamvol262@gmail.com>
2024-10-09 18:32:19 -0600

    implementacion de el servicio validacion

---
+++ src/main/java/gfc/gesationdeinvetario/services/ValidacionService.java
@@ -0,0 +1,21 @@
+package gfc.gesationdeinvetario.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ValidacionService {
+
+    public ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
