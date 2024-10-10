be8a867f2b50b1eeb8b6deed888df463ce5c5f88
Gustavo M <gutamvol262@gmail.com>
2024-10-09 19:08:56 -0600

    pagina para informacion de la transaccion

---
+++ src/app/transaccion/pages/trasanccion-page/trasanccion-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trasanccion-page',
+  templateUrl: './trasanccion-page.component.html',
+  styleUrl: './trasanccion-page.component.css'
+})
+export class TrasanccionPageComponent {
+
+}
