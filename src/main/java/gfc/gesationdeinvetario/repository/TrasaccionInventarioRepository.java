632130d5e69b11edfe1be701cc5c762eab967f25
Gustavo M <gutamvol262@gmail.com>
2025-03-31 21:01:18 -0600

    creando consultas personalizadas

--- src/main/java/gfc/gesationdeinvetario/repository/TrasaccionInventarioRepository.java
+++ src/main/java/gfc/gesationdeinvetario/repository/TrasaccionInventarioRepository.java
@@ -1,7 +1,19 @@
 package gfc.gesationdeinvetario.repository;
 
 import gfc.gesationdeinvetario.entity.Transaccion_inventario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TrasaccionInventarioRepository extends CrudRepository<Transaccion_inventario, Integer> {
+
+    @Query("select t from Transaccion_inventario t where t.id=:productoId")
+    List<Transaccion_inventario>obtenerTransaccionesProducto(@Param("productoId")Integer productoId);
+
+    @Query("select sum (t.cantidad) from Transaccion_inventario t where t.id=:productoId")
+    Integer ObtenerCalculo(@Param("productoId")Integer productoId);
+
+
 }
