2ee7c56c231e6f7f7132d968b14114e3267d7f6a
Gustavo M <gutamvol262@gmail.com>
2025-03-31 21:00:17 -0600

    haciendo consultas personalizadas

--- src/main/java/gfc/gesationdeinvetario/repository/ProductoRepository.java
+++ src/main/java/gfc/gesationdeinvetario/repository/ProductoRepository.java
@@ -2,6 +2,12 @@ package gfc.gesationdeinvetario.repository;
 
 import gfc.gesationdeinvetario.entity.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Productos,Integer> {
+
+    @Query("select p.nombre,c.nombre from Productos p join p.categoria c")
+    List<Object[]>ObtenerProductosConCategoria();
 }
